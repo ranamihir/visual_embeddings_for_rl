a40fc5a9a989ff923436e08f7dd29e65196b34eb
Mihir Rana <ranamihir@gmail.com>
2018-09-26 16:41:24 -0400

    Renamed file.

--- main.py
+++ main.py
@@ -32,19 +32,7 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 data = np.load('/home/mihir/Desktop/GitHub/nyu/capstone_project/data/mnist_test_seq.npy')
 data = np.swapaxes(data, 0, 1)
 
-
 train_loader, val_loader, test_loader = generate_dataloader(data, TEST_SIZE, VAL_SIZE, BATCH_SIZE)
-
-
-# def imshow(data_loader):
-#     data_iter = iter(data_loader)
-#     images = data_iter.next()
-
-#     images = make_grid(images[0].reshape(-1, 1, 64, 64), nrow=10)
-#     np_image = images.numpy()
-
-#     plt.figure(figsize=(50, 20))
-#     plt.imshow(np.transpose(np_image, axes=(1, 2, 0)))
 
 
 if DATASET == 'moving_mnist':
@@ -63,67 +51,6 @@ classification_network = ClassificationNetwork(num_inputs, num_outputs).to(DEVIC
 criterion_train = nn.CrossEntropyLoss()
 criterion_test = nn.CrossEntropyLoss(reduction='sum')
 optimizer = optim.SGD(list(embedding_network.parameters()) + list(classification_network.parameters()), lr=LR)
-
-
-# def train(embedding_network, classification_network, dataloader, criterion, optimizer, epoch):
-#     embedding_network.train()
-#     classification_network.train()
-#     loss_train = 0.
-#     for batch_idx, (x1, x2, y) in enumerate(dataloader):
-#         x1, x2, y = Variable(x1).to(DEVICE), Variable(x2).to(DEVICE), Variable(y).to(DEVICE)
-#         optimizer.zero_grad()
-#         embedding_output1 = embedding_network(x1)
-#         embedding_output2 = embedding_network(x2)
-#         classification_input = torch.dot(embedding_output1, embedding_output2)
-#         classification_output = classification_network(classification_input)
-#         loss = criterion(classification_output, y)
-#         loss.backward()
-#         optimizer.step()
-
-#         # Accurately compute loss, because of different batch size
-#         loss_train += loss.item() * len(x) / len(dataloader.dataset)
-
-#         if batch_idx % 100 == 0:
-#             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-#                 epoch, batch_idx * len(x), len(dataloader.dataset),
-#                 100. * batch_idx / len(dataloader), loss.item()))
-
-#     optimizer.zero_grad()
-#     return loss_train
-
-# def test(embedding_network, classification_network, dataloader, criterion):
-#     embedding_network.eval()
-#     classification_network.eval()
-#     loss_test = 0.
-#     y_ls = []
-#     output_ls = []
-#     with torch.no_grad():
-#         for batch_idx, (x1, x2, y) in enumerate(dataloader):
-#             x1, x2, y = Variable(x1).to(DEVICE), Variable(x2).to(DEVICE), Variable(y).to(DEVICE)
-#             embedding_output1 = embedding_network(x1)
-#             embedding_output2 = embedding_network(x2)
-#             classification_input = torch.dot(embedding_output1, embedding_output2)
-#             classification_output = classification_network(classification_input)
-#             loss = criterion(classification_output, y)
-
-#             # Accurately compute loss, because of different batch size
-#             loss_test += loss.item() / len(dataloader.dataset)
-
-#             output_ls.append(classification_output)
-#             y_ls.append(y)
-#     optimizer.zero_grad()
-#     return loss_test, torch.cat(output_ls, dim=0), torch.cat(y_ls, dim=0)
-
-# def accuracy(embedding_network, classification_network, dataloader, criterion):
-#     _, y_predicted, y_true = test(
-#         embedding_network=embedding_network,
-#         classification_network=classification_network,
-#         dataloader=dataloader,
-#         criterion=criterion
-#     )
-#     y_predicted = y_predicted.max(1)[1]
-#     return 100*y_predicted.eq(y_true.data.view_as(y_predicted)).float().mean().item()
-
 
 for epoch in range(1, N_EPOCHS+1):
     train_loss = train(
