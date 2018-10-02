32ba86845f0930a97df5767d47df1ff30160d203
avidkenil <avidkenil@gmail.com>
2018-10-02 14:30:50 -0400

    changes to make the new data work

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import argparse
+import pickle
 
 import torch
 import torch.nn as nn
@@ -27,7 +28,7 @@ parser.add_argument('--dataset', metavar='DATASET', dest='dataset', help='name o
 parser.add_argument('--batch-size', metavar='BATCH_SIZE', dest='batch_size', help='batch size', required=False, type=int, default=128)
 parser.add_argument('--epochs', metavar='EPOCHS', dest='epochs', help='number of epochs', required=False, type=int, default=10)
 parser.add_argument('--device', metavar='DEVICE', dest='device', help='device', required=False)
-parser.add_argument('--device-id', metavar='DEVICE_ID', dest='device_id', help='device id of gpu', required=False, type=int, default=2)
+parser.add_argument('--device-id', metavar='DEVICE_ID', dest='device_id', help='device id of gpu', required=False, type=int)
 parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--lr', metavar='LR', dest='lr', help='learning rate', required=False, type=float, default=1e-4)
 args = parser.parse_args()
@@ -49,7 +50,7 @@ if args.device_id and DEVICE == 'cuda':
     torch.cuda.set_device(args.device_id)
 
 NUM_PASSES_FOR_GENERATION = 1   # number of passes through data for pair generation
-NUM_FRAMES_IN_STACK = 2         # number of (total) frames to concatenate for each video
+NUM_FRAMES_IN_STACK = 4         # number of (total) frames to concatenate for each video
 NUM_PAIRS_PER_EXAMPLE = 1       # number of pairs to generate for given video and time difference
 TIME_BUCKETS = [[0], [1], [2], [3,4], list(range(5,11,1)), list(range(11,20-NUM_FRAMES_IN_STACK+1,1))]
 
@@ -59,14 +60,14 @@ def main():
     X, y = get_paired_data(PROJECT_DIR, DATA_DIR, PLOTS_DIR, DATASET, TIME_BUCKETS, NUM_PASSES_FOR_GENERATION, NUM_PAIRS_PER_EXAMPLE, NUM_FRAMES_IN_STACK, force=False)
     train_loader, val_loader, test_loader = generate_dataloader(X, y, TEST_SIZE, VAL_SIZE, BATCH_SIZE, PROJECT_DIR, PLOTS_DIR)
 
-    if DATASET == 'mnist_test_seq.npy':
-        in_dim, in_channels, out_dim = 64, NUM_FRAMES_IN_STACK, 1024
-        embedding_hidden_size, classification_hidden_size = 1024, 1024
-        num_outputs = 6
-    elif DATASET == 'cifar10':
-        in_dim, in_channels, out_dim = 32, 3, 1024
-        embedding_hidden_size, classification_hidden_size = 1024, 1024
-        num_outputs = 10
+    #if DATASET == 'mnist_test_seq.npy':
+    in_dim, in_channels, out_dim = 64, NUM_FRAMES_IN_STACK, 1024
+    embedding_hidden_size, classification_hidden_size = 1024, 1024
+    num_outputs = 6
+    # elif DATASET == 'cifar10':
+    #     in_dim, in_channels, out_dim = 32, 3, 1024
+    #     embedding_hidden_size, classification_hidden_size = 1024, 1024
+    #     num_outputs = 10
 
     embedding_network = EmbeddingNetwork(in_dim, in_channels, out_dim, embedding_hidden_size).to(DEVICE)
     classification_network = ClassificationNetwork(out_dim, num_outputs, classification_hidden_size).to(DEVICE)
@@ -83,6 +84,9 @@ def main():
     train_loss_history = []
     val_loss_history = []
 
+
+    accuracy_val_history = []
+    accuracy_train_history = []
     for epoch in range(1, N_EPOCHS+1):
         try:
             train_loss = train(
@@ -107,6 +111,8 @@ def main():
             accuracy_val = accuracy(embedding_network, classification_network, val_loader, criterion_test, DEVICE)
             train_loss_history.append(train_loss)
             val_loss_history.append(val_loss)
+            accuracy_train_history.append(accuracy_train)
+            accuracy_val_history.append(accuracy_val)
 
             print('TRAIN Epoch: {}\tAverage loss: {:.4f}, Accuracy: {:.0f}%'.format(epoch, train_loss, accuracy_train))
             print('VAL  Epoch: {}\tAverage loss: {:.4f}, Accuracy: {:.0f}%\n'.format(epoch, val_loss, accuracy_val))
@@ -120,8 +126,11 @@ def main():
     loss_history_df = pd.DataFrame({
         'train': train_loss_history,
         'test': val_loss_history,
+        'accuracy_train': accuracy_train,
+        'accuracy_val': accuracy_val
     })
 
+    pickle.dump(loss_history_df,open('loss_history_df.pkl','wb'))
     # # Save the model checkpoint
     # checkpoint_path = os.path.join(CHECKPOINTS_DIR, 'checkpoints')
     # torch.save(embedding_network.state_dict(), os.path.join(checkpoint_path, 'embedding_network.ckpt'))
