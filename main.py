5f8e1abd2d3c71ef5b15832be3c8ea1947b25c80
Mihir Rana <ranamihir@gmail.com>
2018-10-02 00:11:49 -0400

    Added functionality for multiple GPUs.

--- main.py
+++ main.py
@@ -24,23 +24,29 @@ from capstone_project.utils import train, test, accuracy, save_plot
 parser = argparse.ArgumentParser()
 parser.add_argument('--project-dir', metavar='PROJECT_DIR', dest='project_dir', help='path to project directory', required=False)
 parser.add_argument('--dataset', metavar='DATASET', dest='dataset', help='name of dataset file in data directory', required=False)
-parser.add_argument('--batch-size', metavar='BATCH_SIZE', dest='batch_size', help='batch size', required=False, type=int, default=64)
-parser.add_argument('--epochs', metavar='EPOCHS', dest='epochs', help='number of epochs', required=False, type=int, default=50)
+parser.add_argument('--batch-size', metavar='BATCH_SIZE', dest='batch_size', help='batch size', required=False, type=int, default=128)
+parser.add_argument('--epochs', metavar='EPOCHS', dest='epochs', help='number of epochs', required=False, type=int, default=10)
 parser.add_argument('--device', metavar='DEVICE', dest='device', help='device', required=False)
+parser.add_argument('--device-id', metavar='DEVICE_ID', dest='device_id', help='device id of gpu', required=False, type=int, default=2)
+parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--lr', metavar='LR', dest='lr', help='learning rate', required=False, type=float, default=1e-4)
 args = parser.parse_args()
 
 
 # Globals
 PROJECT_DIR = args.project_dir if args.project_dir else '/home/mihir/Desktop/GitHub/nyu/capstone_project/'
-DATA_DIR, PLOTS_DIR = 'data', 'plots'
+DATA_DIR, CHECKPOINTS_DIR, PLOTS_DIR = 'data', 'checkpoints', 'plots'
 DATASET = args.dataset if args.dataset else 'mnist_test_seq.npy'
 TEST_SIZE, VAL_SIZE = 0.2, 0.2
 
 BATCH_SIZE = args.batch_size    # input batch size for training
 N_EPOCHS = args.epochs          # number of epochs to train
 LR = args.lr                    # learning rate
+NGPU = args.ngpu                # number of GPUs
 DEVICE = args.device if args.device else 'cuda' if torch.cuda.is_available() else 'cpu'
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if args.device_id and DEVICE == 'cuda':
+    torch.cuda.set_device(args.device_id)
 
 NUM_PASSES_FOR_GENERATION = 1   # number of passes through data for pair generation
 NUM_FRAMES_IN_STACK = 2         # number of (total) frames to concatenate for each video
@@ -64,6 +70,12 @@ def main():
 
     embedding_network = EmbeddingNetwork(in_dim, in_channels, out_dim, embedding_hidden_size).to(DEVICE)
     classification_network = ClassificationNetwork(out_dim, num_outputs, classification_hidden_size).to(DEVICE)
+    # if torch.cuda.device_count() > 1:
+    #     print("Let's use", torch.cuda.device_count(), "GPUs!")
+    #     embedding_network = nn.DataParallel(embedding_network)
+    #     embedding_network.to(device)
+    #     classification_network = nn.DataParallel(classification_network)
+    #     classification_network.to(device)
     criterion_train = nn.CrossEntropyLoss()
     criterion_test = nn.CrossEntropyLoss(reduction='sum')
     optimizer = optim.Adam(list(embedding_network.parameters()) + list(classification_network.parameters()), lr=LR)
@@ -100,12 +112,22 @@ def main():
             print('VAL  Epoch: {}\tAverage loss: {:.4f}, Accuracy: {:.0f}%\n'.format(epoch, val_loss, accuracy_val))
         except KeyboardInterrupt:
             print('Keyboard Interrupted!')
+            # Save the model checkpoint
+            # torch.save(embedding_network.state_dict(), 'embedding_network_{}.ckpt'.format(epoch))
+            # torch.save(classification_network.state_dict(), 'classification_network_{}.ckpt'.format(epoch))
             break
 
     loss_history_df = pd.DataFrame({
         'train': train_loss_history,
         'test': val_loss_history,
     })
+
+    # # Save the model checkpoint
+    # checkpoint_path = os.path.join(CHECKPOINTS_DIR, 'checkpoints')
+    # torch.save(embedding_network.state_dict(), os.path.join(checkpoint_path, 'embedding_network.ckpt'))
+    # torch.save(classification_network.state_dict(), os.path.join(checkpoint_path, 'classification_network.ckpt'))
+    # model.load_state_dict(torch.load(os.path.join(checkpoint_path, embedding_network), strict=True))
+    # model.load_state_dict(torch.load(os.path.join(checkpoint_path, classification_network), strict=True))
 
     fig = plt.figure()
     loss_history_df.plot(alpha=0.5, figsize=(10,8))
