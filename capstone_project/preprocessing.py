483ef94b09f5a1d488d92ba695bc7a244320ac1b
Mihir Rana <ranamihir@gmail.com>
2018-10-14 01:44:25 -0400

    Restructured dataloader.

--- capstone_project/preprocessing.py
+++ capstone_project/preprocessing.py
@@ -4,156 +4,187 @@ import os
 import pickle
 import logging
 
+from sklearn.model_selection import train_test_split
 import torch
 from torchvision import transforms
 from torch.utils.data.dataset import Dataset
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 
 from capstone_project.utils import imshow, save_object, load_object
 
-def load_data(project_dir, data_dir, filename):
-    logging.info('Loading "{}"...'.format(filename))
-    filename = os.path.join(project_dir, data_dir, filename)
-    data = np.load(filename)
-    data = data.swapaxes(0,1)
-    logging.info('Done.')
-    return data
-
-def get_time_buckets_dict(time_buckets):
-    '''
-    Returns a dict, with the time diff
-    as its key and the target class (0-indexed) for it as its value
-    '''
-    logging.info('Getting time buckets dictionary...')
-    bucket_idx = 0
-    buckets_dict = {}
-    for bucket in time_buckets:
-        for time in bucket:
-            buckets_dict[time] = bucket_idx
-        bucket_idx += 1
-    logging.info('Done.')
-    return buckets_dict
-
-def get_frame_differences_dict(num_total_frames, max_frame_diff, num_frames_in_stack):
-    '''
-    Returns a dict with the key as the time difference between the frames
-    and the value as a list of tuples (start_frame, end_frame) containing
-    all the pair of frames with that diff in time
-    '''
-    logging.info('Getting frame differences dictionary...')
-    differences_dict = {}
-    differences = range(max_frame_diff+1)
-    for diff in differences:
-        start_frame = num_frames_in_stack-1
-        end_frame = start_frame+diff
-        while end_frame <= num_total_frames-1:
-            differences_dict.setdefault(diff, []).append(tuple((start_frame, end_frame)))
-            start_frame += 1
-            end_frame += 1
-    print('Done.')
-    return differences_dict
-
-def get_samples_at_difference(data, difference, differences_dict, num_pairs_per_example, num_frames_in_stack, time_buckets_dict):
-    '''
-    The task of this function is to get the samples by first selecting the list of tuples
-    for the associated time difference, and then sampling the num of pairs per video example
-    and then finally returning, the video pairs and their associated class(for the time bucket)
-    '''
-    logging.info('Getting all pairs with a frame difference of {}...'.format(difference))
-    video_pairs, y = [], []
-    candidates = differences_dict[difference]
-    np.random.seed(1337)
-    idx_pairs = np.random.choice(len(candidates), size=num_pairs_per_example)
-    for row in data:
-        for idx_pair in idx_pairs:
-            target1_last_frame, target2_last_frame = candidates[idx_pair]
-            target1_frames = list(range(target1_last_frame-num_frames_in_stack+1, target1_last_frame+1))
-            target2_frames = list(range(target2_last_frame-num_frames_in_stack+1, target2_last_frame+1))
-            video_pairs.append([row[target1_frames], row[target2_frames]])
-            bucket = time_buckets_dict[difference]
-            y.append(bucket)
-    logging.info('Done.')
-    return np.array(video_pairs), np.array(y)
-
-def get_paired_data(project_dir, data_dir, plots_dir, filename, time_buckets, num_passes_for_generation=2, num_pairs_per_example=1, num_frames_in_stack=2, force=False):
-    mean, std = 0, 1
-    data = load_data(project_dir, data_dir, filename)
-    imshow(data, mean, std, project_dir, plots_dir)
-
-    num_total_frames = data.shape[1]
-    max_frame_diff = np.hstack([bucket for bucket in time_buckets]).max()
-    assert max_frame_diff <= num_total_frames-num_frames_in_stack, \
-        'Cannot have difference of {} when sequence length is {} and number of \
-        stacked frames are {}'.format(max_frame_diff, num_total_frames, num_frames_in_stack)
-
-    filename = '.'.join(filename.split('.')[:-1])
-    X_path = os.path.join(project_dir, data_dir, '{}_X.pkl'.format(filename))
-    y_path = os.path.join(project_dir, data_dir, '{}_y.pkl'.format(filename))
-    if not force and os.path.exists(X_path) and os.path.exists(y_path):
-        data = None
-        logging.info('Found existing data. Loading it...')
-        X = load_object(X_path)
-        y = load_object(y_path)
-        logging.info('Done.')
-        return X, y
-    logging.info('Did not find existing data. Creating it...')
-    time_buckets_dict = get_time_buckets_dict(time_buckets)
-    differences_dict = get_frame_differences_dict(num_total_frames, max_frame_diff, num_frames_in_stack)
-    X, y = np.array([]), np.array([])
-    for i in range(num_passes_for_generation):
-        logging.info('Making pass {} through data...'.format(i+1))
-        for difference in range(max_frame_diff+1):
-            video_pairs, targets = get_samples_at_difference(data, difference, differences_dict, num_pairs_per_example, num_frames_in_stack, time_buckets_dict)
-            X = np.vstack((X, video_pairs)) if X.size else video_pairs
-            y = np.append(y, targets)
-        logging.info('Done.')
-    logging.info('Data generation done. Dumping data to disk...')
-    save_object(X, X_path)
-    save_object(y, y_path)
-    logging.info('Done.')
-    return X, y
 
 class MovingMNISTDataset(Dataset):
-    # TODO: write function for implementing transforms
-    def __init__(self, X, y, transforms=None):
-        self.X = X
-        self.y = y
-        self.transforms = transforms
+	# TODO: write function for implementing transforms
+	def __init__(self, X, y, transforms=None):
+		self.X = X
+		self.y = y
+		self.transforms = transforms
 
-    def __getitem__(self, index):
-        # Load data and get label
-        x1 = self.X[index][0]
-        x2 = self.X[index][1]
-        y = self.y[index]
-        return x1, x2, y
+	def __getitem__(self, index):
+		# if self.transforms is not None:
+		#     data = self.transforms(data)
 
-    def __len__(self):
-        return len(self.y)
+		# Load data and get label
+		x1 = self.X[index][0]
+		x2 = self.X[index][1]
+		y = self.y[index]
+		return x1, x2, y
 
-def generate_dataloader(X, y, test_size, val_size, batch_size, project_dir, plots_dir):
-    # TODO: Normalize data
-    # mean = np.mean(dataset)
-    # std = np.std(dataset)
-    # dataset = (dataset - mean)/std
-    # data_transform = transforms.Compose([
-    #     transforms.ToTensor(),
-    #     transforms.Normalize((mean,), (std,))
-    # ])
-    logging.info('Generating train, val, and test data loaders...')
-    X, y = torch.from_numpy(X), torch.from_numpy(y)
-    dataset = MovingMNISTDataset(X, y, transforms=None)
+	def __len__(self):
+		return len(self.y)
 
-    num_test = int(np.floor(test_size*len(dataset)))
-    num_train_val = len(dataset) - num_test
-    num_val = int(np.floor(num_train_val*val_size/(1 - test_size)))
-    num_train = num_train_val - num_val
+def generate_dataloaders(project_dir, data_dir, plots_dir, filename, time_buckets, batch_size, num_passes_for_generation=1, num_pairs_per_example=1, num_frames_in_stack=2, val_size=0.2, test_size=0.2, force=False):
+	data_path = os.path.join(project_dir, data_dir, '{}_{}.pkl')
+	train_path = data_path.format(filename, 'train')
+	val_path = data_path.format(filename, 'val')
+	test_path = data_path.format(filename, 'test')
 
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [num_train, num_val, num_test])
+	if not force and os.path.exists(train_path) and os.path.exists(val_path) and os.path.exists(test_path):
+		logging.info('Found all data sets on disk. Loading them...')
+		data_loaders = []
+		for dataset_type in ['train', 'val', 'test']:
+			dataset = load_object(data_path.format(filename, dataset_type))
+			data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True if dataset_type == 'train' else False, num_workers=2)
+			data_loaders.append(data_loader)
+		logging.info('Done.')
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-    test_loader  = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+	else:
+		logging.info('Did not find at least one data set on disk. Creating all 3...')
 
-    logging.info('Done.')
+		logging.info('Loading "{}"...'.format(filename))
+		data = load_data(project_dir, data_dir, filename)
+		logging.info('Done.')
 
-    return train_loader, val_loader, test_loader
+		num_total_frames = data.shape[1]
+		max_frame_diff = np.hstack([bucket for bucket in time_buckets]).max()
+		assert max_frame_diff <= num_total_frames-num_frames_in_stack, \
+			'Cannot have difference of {} when sequence length is {} and number of \
+			stacked frames are {}'.format(max_frame_diff, num_total_frames, num_frames_in_stack)
+
+		logging.info('Splitting data set into train, val, and test sets...')
+		data_dict = split_data(data, val_size, test_size, project_dir, data_dir, filename)
+		logging.info('Done.')
+
+		mean, std = 0, 1 # Calculate mean, std from train data
+		# TODO: Normalize data
+		# mean = np.mean(dataset)
+		# std = np.std(dataset)
+		# dataset = (dataset - mean)/std
+		# data_transform = transforms.Compose([
+		#     transforms.ToTensor(),
+		#     transforms.Normalize((mean,), (std,))
+		# ])
+
+		imshow(data_dict['train'], mean, std, project_dir, plots_dir)
+
+		time_buckets_dict = get_time_buckets_dict(time_buckets)
+		differences_dict = get_frame_differences_dict(num_total_frames, max_frame_diff, num_frames_in_stack)
+
+		data_loaders = []
+		for dataset_type in ['train', 'val', 'test']:
+			logging.info('Generating {} data set...'.format(dataset_type))
+			X, y = np.array([]), np.array([])
+			for i in range(num_passes_for_generation):
+				logging.info('Making pass {} through data...'.format(i+1))
+				for difference in range(max_frame_diff+1):
+					video_pairs, targets = get_samples_at_difference(data_dict[dataset_type], difference, differences_dict, num_pairs_per_example, num_frames_in_stack, time_buckets_dict)
+					X = np.vstack((X, video_pairs)) if X.size else video_pairs
+					y = np.append(y, targets)
+				logging.info('Done.')
+
+			logging.info('Done. Generating {} data loader...'.format(dataset_type))
+			X, y = torch.from_numpy(X), torch.from_numpy(y)
+			dataset = MovingMNISTDataset(X, y, transforms=None)
+			logging.info('Done. Dumping data loader to disk...')
+			save_object(dataset, data_path.format(filename, dataset_type))
+			logging.info('Done.')
+
+			data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+			data_loaders.append(data_loader)
+
+	return data_loaders
+
+def load_data(project_dir, data_dir, filename):
+	filename = os.path.join(project_dir, data_dir, filename)
+	data = np.load(filename)
+	data = data.swapaxes(0,1)
+	return data
+
+def split_data(data, val_size, test_size, project_dir, data_dir, filename):
+	num_test = int(np.floor(test_size*len(data)))
+	num_train_val = len(data) - num_test
+	num_val = int(np.floor(num_train_val*val_size/(1 - test_size)))
+
+	train_data, test_data = train_test_split(data, test_size=num_test)
+	train_data, val_data = train_test_split(train_data, test_size=num_val)
+
+	filename, extension = os.path.splitext(filename)
+	save_object(train_data, os.path.join(project_dir, data_dir, '{}_train.pkl'.format(filename)))
+	save_object(val_data, os.path.join(project_dir, data_dir, '{}_val.pkl'.format(filename)))
+	save_object(test_data, os.path.join(project_dir, data_dir, '{}_test.pkl'.format(filename)))
+
+	data_dict = {
+		'train': train_data,
+		'val': val_data,
+		'test': test_data
+	}
+
+	data = None # Free data
+
+	return data_dict
+
+def get_time_buckets_dict(time_buckets):
+	'''
+	Returns a dict, with the time diff
+	as its key and the target class (0-indexed) for it as its value
+	'''
+	logging.info('Getting time buckets dictionary...')
+	bucket_idx = 0
+	buckets_dict = {}
+	for bucket in time_buckets:
+		for time in bucket:
+			buckets_dict[time] = bucket_idx
+		bucket_idx += 1
+	logging.info('Done.')
+	return buckets_dict
+
+def get_frame_differences_dict(num_total_frames, max_frame_diff, num_frames_in_stack):
+	'''
+	Returns a dict with the key as the time difference between the frames
+	and the value as a list of tuples (start_frame, end_frame) containing
+	all the pair of frames with that diff in time
+	'''
+	logging.info('Getting frame differences dictionary...')
+	differences_dict = {}
+	differences = range(max_frame_diff+1)
+	for diff in differences:
+		start_frame = num_frames_in_stack-1
+		end_frame = start_frame+diff
+		while end_frame <= num_total_frames-1:
+			differences_dict.setdefault(diff, []).append(tuple((start_frame, end_frame)))
+			start_frame += 1
+			end_frame += 1
+	print('Done.')
+	return differences_dict
+
+def get_samples_at_difference(data, difference, differences_dict, num_pairs_per_example, num_frames_in_stack, time_buckets_dict):
+	'''
+	The task of this function is to get the samples by first selecting the list of tuples
+	for the associated time difference, and then sampling the num of pairs per video example
+	and then finally returning, the video pairs and their associated class(for the time bucket)
+	'''
+	logging.info('Getting all pairs with a frame difference of {}...'.format(difference))
+	video_pairs, y = [], []
+	candidates = differences_dict[difference]
+	np.random.seed(1337)
+	idx_pairs = np.random.choice(len(candidates), size=num_pairs_per_example)
+	for row in data:
+		for idx_pair in idx_pairs:
+			target1_last_frame, target2_last_frame = candidates[idx_pair]
+			target1_frames = list(range(target1_last_frame-num_frames_in_stack+1, target1_last_frame+1))
+			target2_frames = list(range(target2_last_frame-num_frames_in_stack+1, target2_last_frame+1))
+			video_pairs.append([row[target1_frames], row[target2_frames]])
+			bucket = time_buckets_dict[difference]
+			y.append(bucket)
+	logging.info('Done.')
+	return np.array(video_pairs), np.array(y)
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from torchviz import make_dot, make_dot_from_trace
 
-from capstone_project.preprocessing import get_paired_data, generate_dataloader
+from capstone_project.preprocessing import generate_dataloaders
 from capstone_project.models.embedding_network import EmbeddingNetwork
 from capstone_project.models.classification_network import ClassificationNetwork
 from capstone_project.utils import *
@@ -27,10 +27,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--project-dir', metavar='PROJECT_DIR', dest='project_dir', help='path to project directory', required=False)
 parser.add_argument('--dataset', metavar='DATASET', dest='dataset', help='name of dataset file in data directory', required=False)
 parser.add_argument('--data-dir', metavar='DATA_DIR', dest='data_dir', help='path to data directory (used if different from "data")', \
-                    required=False, default='data')
+					required=False, default='data')
 parser.add_argument('--force', action='store_true', help='overwrites all existing data')
 parser.add_argument('--checkpoints-dir', metavar='CHECKPOINTS_DIR', dest='checkpoints_dir', help='path to checkpoints directory', \
-                    required=False, default='checkpoints') # TODO: load-ckpt
+					required=False, default='checkpoints') # TODO: load-ckpt
 parser.add_argument('--batch-size', metavar='BATCH_SIZE', dest='batch_size', help='batch size', required=False, type=int, default=64)
 parser.add_argument('--epochs', metavar='EPOCHS', dest='epochs', help='number of epochs', required=False, type=int, default=10)
 parser.add_argument('--device', metavar='DEVICE', dest='device', help='device', required=False)
@@ -38,7 +38,7 @@ parser.add_argument('--device-id', metavar='DEVICE_ID', dest='device_id', help='
 parser.add_argument('--ngpu', metavar='NGPU', dest='ngpu', help='number of GPUs to use', required=False, type=int, default=1)
 parser.add_argument('--lr', metavar='LR', dest='lr', help='learning rate', required=False, type=float, default=1e-4)
 parser.add_argument('--num-passes', metavar='NUM_PASSES_FOR_GENERATION', dest='num_passes', help='number of passes through data to generate pairs', \
-                    required=False, type=int, default=1)
+					required=False, type=int, default=1)
 parser.add_argument('--num-frames', metavar='NUM_FRAMES_IN_STACK', dest='num_frames', help='number of stacked frames', required=False, type=int, default=2)
 parser.add_argument('--num-pairs', metavar='NUM_PAIRS_PER_EXAMPLE', dest='num_pairs', help='number of pairs per video', required=False, type=int, default=5)
 args = parser.parse_args()
@@ -58,8 +58,8 @@ NGPU = args.ngpu                # number of GPUs
 # TODO: Incorporate NGPUs
 DEVICE = args.device if args.device else 'cuda' if torch.cuda.is_available() else 'cpu'
 if args.device_id and DEVICE == 'cuda':
-    DEVICE_ID = args.device_id
-    torch.cuda.set_device(DEVICE_ID)
+	DEVICE_ID = args.device_id
+	torch.cuda.set_device(DEVICE_ID)
 
 NUM_PASSES_FOR_GENERATION = args.num_passes   # number of passes through data for pair generation
 NUM_FRAMES_IN_STACK = args.num_frames         # number of (total) frames to concatenate for each video
@@ -67,109 +67,111 @@ NUM_PAIRS_PER_EXAMPLE = args.num_pairs        # number of pairs to generate for 
 TIME_BUCKETS = [[0], [1], [2], [3,4], range(5,11,1)]
 
 def main():
-    make_dirs(PROJECT_DIR, [CHECKPOINTS_DIR, PLOTS_DIR, LOGGING_DIR]) # Create all required directories if not present
-    setup_logging(PROJECT_DIR, LOGGING_DIR) # Setup configuration for logging
-    print_config(globals().copy()) # Print all global variables defined above
+	make_dirs(PROJECT_DIR, [CHECKPOINTS_DIR, PLOTS_DIR, LOGGING_DIR]) # Create all required directories if not present
+	setup_logging(PROJECT_DIR, LOGGING_DIR) # Setup configuration for logging
+	print_config(globals().copy()) # Print all global variables defined above
 
-    # TODO: make graphviz work
+	# TODO: make graphviz work
 
-    X, y = get_paired_data(PROJECT_DIR, DATA_DIR, PLOTS_DIR, DATASET, TIME_BUCKETS, NUM_PASSES_FOR_GENERATION, NUM_PAIRS_PER_EXAMPLE, NUM_FRAMES_IN_STACK, force=args.force)
-    train_loader, val_loader, test_loader = generate_dataloader(X, y, TEST_SIZE, VAL_SIZE, BATCH_SIZE, PROJECT_DIR, PLOTS_DIR)
+	train_loader, val_loader, test_loader = generate_dataloaders(PROJECT_DIR, DATA_DIR, PLOTS_DIR, DATASET, TIME_BUCKETS, \
+																BATCH_SIZE, NUM_PASSES_FOR_GENERATION, NUM_PAIRS_PER_EXAMPLE, \
+																NUM_FRAMES_IN_STACK, VAL_SIZE, TEST_SIZE, force=args.force)
+	exit()
 
-    # Network hyperparameters
-    in_dim, in_channels, out_dim = X.shape[-1], NUM_FRAMES_IN_STACK, 1024
-    embedding_hidden_size, classification_hidden_size = 1024, 1024
-    num_outputs = len(TIME_BUCKETS)
+	# Network hyperparameters
+	in_dim, in_channels, out_dim = X.shape[-1], NUM_FRAMES_IN_STACK, 1024
+	embedding_hidden_size, classification_hidden_size = 1024, 1024
+	num_outputs = len(TIME_BUCKETS)
 
-    logging.info('Creating models...')
-    embedding_network = EmbeddingNetwork(in_dim, in_channels, embedding_hidden_size, out_dim).to(DEVICE)
-    classification_network = ClassificationNetwork(out_dim, classification_hidden_size, num_outputs).to(DEVICE)
-    logging.info('Done.')
-    # if torch.cuda.device_count() > 1:
-    #     logging.info("Let's use", torch.cuda.device_count(), "GPUs!")
-    #     embedding_network = nn.DataParallel(embedding_network)
-    #     embedding_network.to(device)
-    #     classification_network = nn.DataParallel(classification_network)
-    #     classification_network.to(device)
-    criterion_train = nn.CrossEntropyLoss()
-    criterion_test = nn.CrossEntropyLoss(reduction='sum')
-    optimizer = optim.Adam(list(embedding_network.parameters()) + list(classification_network.parameters()), lr=LR)
+	logging.info('Creating models...')
+	embedding_network = EmbeddingNetwork(in_dim, in_channels, embedding_hidden_size, out_dim).to(DEVICE)
+	classification_network = ClassificationNetwork(out_dim, classification_hidden_size, num_outputs).to(DEVICE)
+	logging.info('Done.')
+	# if torch.cuda.device_count() > 1:
+	#     logging.info("Let's use", torch.cuda.device_count(), "GPUs!")
+	#     embedding_network = nn.DataParallel(embedding_network)
+	#     embedding_network.to(device)
+	#     classification_network = nn.DataParallel(classification_network)
+	#     classification_network.to(device)
+	criterion_train = nn.CrossEntropyLoss()
+	criterion_test = nn.CrossEntropyLoss(reduction='sum')
+	optimizer = optim.Adam(list(embedding_network.parameters()) + list(classification_network.parameters()), lr=LR)
 
-    train_loss_history, train_accuracy_history = [], []
-    val_loss_history, val_accuracy_history = [], []
-    stop_epoch = N_EPOCHS
+	train_loss_history, train_accuracy_history = [], []
+	val_loss_history, val_accuracy_history = [], []
+	stop_epoch = N_EPOCHS
 
-    # Uncomment this line to load model already dumped
-    # embedding_network, classification_network, optimizer, train_loss_history, val_loss_history = \
-        # load_checkpoint(embedding_network, classification_network, optimizer, DEVICE, 1, CHECKPOINTS_DIR)
+	# Uncomment this line to load model already dumped
+	# embedding_network, classification_network, optimizer, train_loss_history, val_loss_history = \
+		# load_checkpoint(embedding_network, classification_network, optimizer, DEVICE, 1, CHECKPOINTS_DIR)
 
-    for epoch in range(1, N_EPOCHS+1):
-        try:
-            train_loss = train(
-                embedding_network=embedding_network,
-                classification_network=classification_network,
-                criterion=criterion_train,
-                dataloader=train_loader,
-                optimizer=optimizer,
-                device=DEVICE,
-                epoch=epoch
-            )
+	for epoch in range(1, N_EPOCHS+1):
+		try:
+			train_loss = train(
+				embedding_network=embedding_network,
+				classification_network=classification_network,
+				criterion=criterion_train,
+				dataloader=train_loader,
+				optimizer=optimizer,
+				device=DEVICE,
+				epoch=epoch
+			)
 
-            val_loss, val_pred, val_true = test(
-                embedding_network=embedding_network,
-                classification_network=classification_network,
-                dataloader=val_loader,
-                criterion=criterion_test,
-                device=DEVICE
-            )
+			val_loss, val_pred, val_true = test(
+				embedding_network=embedding_network,
+				classification_network=classification_network,
+				dataloader=val_loader,
+				criterion=criterion_test,
+				device=DEVICE
+			)
 
-            accuracy_train = accuracy(embedding_network, classification_network, train_loader, criterion_test, DEVICE)
-            accuracy_val = accuracy(embedding_network, classification_network, val_loader, criterion_test, DEVICE)
-            train_loss_history.append(train_loss)
-            val_loss_history.append(val_loss)
-            train_accuracy_history.append(accuracy_train)
-            val_accuracy_history.append(accuracy_val)
+			accuracy_train = accuracy(embedding_network, classification_network, train_loader, criterion_test, DEVICE)
+			accuracy_val = accuracy(embedding_network, classification_network, val_loader, criterion_test, DEVICE)
+			train_loss_history.append(train_loss)
+			val_loss_history.append(val_loss)
+			train_accuracy_history.append(accuracy_train)
+			val_accuracy_history.append(accuracy_val)
 
-            logging.info('TRAIN Epoch: {}\tAverage loss: {:.4f}, Accuracy: {:.0f}%'.format(epoch, train_loss, accuracy_train))
-            logging.info('VAL   Epoch: {}\tAverage loss: {:.4f}, Accuracy: {:.0f}%\n'.format(epoch, val_loss, accuracy_val))
-        except KeyboardInterrupt:
-            # Save the model checkpoints
-            logging.info('Keyboard Interrupted!')
-            stop_epoch = epoch
-            break
+			logging.info('TRAIN Epoch: {}\tAverage loss: {:.4f}, Accuracy: {:.0f}%'.format(epoch, train_loss, accuracy_train))
+			logging.info('VAL   Epoch: {}\tAverage loss: {:.4f}, Accuracy: {:.0f}%\n'.format(epoch, val_loss, accuracy_val))
+		except KeyboardInterrupt:
+			# Save the model checkpoints
+			logging.info('Keyboard Interrupted!')
+			stop_epoch = epoch
+			break
 
-    # Save the model checkpoint
-    logging.info('Dumping model and results...')
-    save_checkpoint(embedding_network, classification_network, optimizer, train_loss_history, val_loss_history, \
-        train_accuracy_history, val_accuracy_history, stop_epoch, CHECKPOINTS_DIR)
-    logging.info('Done.')
+	# Save the model checkpoint
+	logging.info('Dumping model and results...')
+	save_checkpoint(embedding_network, classification_network, optimizer, train_loss_history, val_loss_history, \
+		train_accuracy_history, val_accuracy_history, stop_epoch, CHECKPOINTS_DIR)
+	logging.info('Done.')
 
-    logging.info('Saving and plotting loss and accuracy histories...')
-    fig = plt.figure()
-    loss_history_df = pd.DataFrame({
-        'train': train_loss_history,
-        'test': val_loss_history,
-    })
-    loss_history_df.plot(alpha=0.5, figsize=(10, 8))
-    save_plot(PROJECT_DIR, PLOTS_DIR, fig, 'loss_vs_iterations.png')
+	logging.info('Saving and plotting loss and accuracy histories...')
+	fig = plt.figure()
+	loss_history_df = pd.DataFrame({
+		'train': train_loss_history,
+		'test': val_loss_history,
+	})
+	loss_history_df.plot(alpha=0.5, figsize=(10, 8))
+	save_plot(PROJECT_DIR, PLOTS_DIR, fig, 'loss_vs_iterations.png')
 
-    fig = plt.figure()
-    accuracy_history_df = pd.DataFrame({
-        'train': train_accuracy_history,
-        'test': val_accuracy_history,
-    })
-    accuracy_history_df.plot(alpha=0.5, figsize=(10, 8))
-    save_plot(PROJECT_DIR, PLOTS_DIR, fig, 'accuracies_vs_iterations.png')
-    logging.info('Done.')
+	fig = plt.figure()
+	accuracy_history_df = pd.DataFrame({
+		'train': train_accuracy_history,
+		'test': val_accuracy_history,
+	})
+	accuracy_history_df.plot(alpha=0.5, figsize=(10, 8))
+	save_plot(PROJECT_DIR, PLOTS_DIR, fig, 'accuracies_vs_iterations.png')
+	logging.info('Done.')
 
-    logging.info('Generating and saving visualization of computational graph...')
-    with torch.onnx.set_training(embedding_network, False) and torch.onnx.set_training(classification_network, False):
-        embedding_output1 = embedding_network(train_loader.dataset[0][0].view(-1, 1, in_dim, in_dim).to(DEVICE).float())
-        embedding_output2 = embedding_network(train_loader.dataset[1][0].view(-1, 1, in_dim, in_dim).to(DEVICE).float())
-        trace, _ = torch.jit.get_trace_graph(classification_network, args=(embedding_output1, embedding_output2,))
-        with open(os.path.join(PROJECT_DIR, PLOTS_DIR, 'model_DAG.svg'), 'w') as f:
-            f.write(make_dot_from_trace(trace)._repr_svg_())
-    logging.info('Done.')
+	logging.info('Generating and saving visualization of computational graph...')
+	with torch.onnx.set_training(embedding_network, False) and torch.onnx.set_training(classification_network, False):
+		embedding_output1 = embedding_network(train_loader.dataset[0][0].view(-1, 1, in_dim, in_dim).to(DEVICE).float())
+		embedding_output2 = embedding_network(train_loader.dataset[1][0].view(-1, 1, in_dim, in_dim).to(DEVICE).float())
+		trace, _ = torch.jit.get_trace_graph(classification_network, args=(embedding_output1, embedding_output2,))
+		with open(os.path.join(PROJECT_DIR, PLOTS_DIR, 'model_DAG.svg'), 'w') as f:
+			f.write(make_dot_from_trace(trace)._repr_svg_())
+	logging.info('Done.')
 
 if __name__ == '__main__':
-    main()
+	main()
