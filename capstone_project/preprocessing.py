Fixed normalization bug.

capstone_project/preprocessing.py
@@ -27,11 +27,14 @@ class MovingMNISTDataset(Dataset):
 		video_idx = np.random.choice(len(self.data))
 		difference = np.random.choice(list(self.time_buckets_dict.keys()))
 
-		(x1, x2), y, difference, (frame1, frame2) = self._get_sample_at_difference(video_idx, difference)
+		(x1, x2), y, difference, frame_numbers = self._get_sample_at_difference(video_idx, difference)
 
 		if self.transforms:
-			x1 = self.transforms(x1)
-			x2 = self.transforms(x2)
+			x1 = torch.stack([self.transforms(x[:,:,np.newaxis]) for x in x1], dim=0).squeeze(1)
+			x2 = torch.stack([self.transforms(x[:,:,np.newaxis]) for x in x2], dim=0).squeeze(1)
+
+		y, difference = torch.from_numpy(y), torch.from_numpy(difference)
+		frame1, frame2 = torch.from_numpy(frame_numbers)
 
 		return x1, x2, y, difference, (frame1, frame2)
 
@@ -101,8 +104,7 @@ class MovingMNISTDataset(Dataset):
 
 		frame_numbers = np.array([image1_last_frame, image2_last_frame])
 
-		return torch.from_numpy(image_pair), torch.from_numpy(bucket), \
-			torch.from_numpy(np.array(difference)), torch.from_numpy(frame_numbers)
+		return image_pair, bucket, np.array(difference), frame_numbers
 
 
 class AtariDataset(Dataset):
@@ -119,11 +121,14 @@ class AtariDataset(Dataset):
 		video_idx = np.random.choice(len(self.data))
 		difference = np.random.choice(list(self.time_buckets_dict.keys()))
 
-		(x1, x2), y, difference, (frame1, frame2) = self._get_sample_at_difference(video_idx, difference)
+		(x1, x2), y, difference, frame_numbers = self._get_sample_at_difference(video_idx, difference)
 
 		if self.transforms:
-			x1 = self.transforms(x1)
-			x2 = self.transforms(x2)
+			x1 = torch.stack([self.transforms(x[:,:,np.newaxis]) for x in x1], dim=0).squeeze(1)
+			x2 = torch.stack([self.transforms(x[:,:,np.newaxis]) for x in x2], dim=0).squeeze(1)
+
+		y, difference = torch.from_numpy(y), torch.from_numpy(difference)
+		frame1, frame2 = torch.from_numpy(frame_numbers)
 
 		return x1, x2, y, difference, (frame1, frame2)
 
@@ -188,12 +193,15 @@ class AtariDataset(Dataset):
 
 		bucket = np.array(self.time_buckets_dict[difference])
 
-		return torch.from_numpy(image_pair), torch.from_numpy(bucket), \
-			torch.from_numpy(np.array(difference)), torch.from_numpy(image_pair_idxs)
+		return image_pair, bucket, np.array(difference), image_pair_idxs
 
 def generate_online_dataloader(project_dir, data_dir, plots_dir, dataset, dataset_size, dataset_type, \
-							time_buckets, batch_size, num_frames_in_stack=2, ext='.npy', transforms=None):
-	data = load_data(project_dir, data_dir, dataset, dataset_type, ext)
+							time_buckets, batch_size, num_frames_in_stack=2, ext='.npy', \
+							transforms=None, data_max=None, data_min=None):
+	if dataset_type == 'train':
+		data, data_max, data_min = load_data(project_dir, data_dir, dataset, dataset_type, ext)
+	else:
+		data = load_data(project_dir, data_dir, dataset, dataset_type, ext, data_max, data_min)
 
 	if 'pong' in dataset:
 		IS_STACKED_DATA = 1
@@ -218,20 +226,20 @@ def generate_online_dataloader(project_dir, data_dir, plots_dir, dataset, datase
 									dataset_size, transforms=transforms)
 
 	shuffle = 1 if dataset_type == 'train' else False
-	dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=8)
+	dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=4)
 	logging.info('Done.')
 
 	if dataset_type == 'train':
-		return dataloader, transforms
+		return dataloader, transforms, data_max, data_min
 	return dataloader
 
 
 class OfflineMovingMNISTDataset(Dataset):
 	def __init__(self, X, y, differences, frame_numbers, transforms=None):
 		self.X = X
-		self.y = y
-		self.differences = differences
-		self.frame_numbers = frame_numbers
+		self.y = torch.from_numpy(y)
+		self.differences = torch.from_numpy(differences)
+		self.frame_numbers = torch.from_numpy(frame_numbers)
 		self.transforms = transforms
 
 	def __getitem__(self, index):
@@ -241,8 +249,8 @@ class OfflineMovingMNISTDataset(Dataset):
 		frame1, frame2 = self.frame_numbers[index]
 
 		if self.transforms:
-			x1 = self.transforms(x1)
-			x2 = self.transforms(x2)
+			x1 = torch.stack([self.transforms(x[:,:,np.newaxis]) for x in x1], dim=0).squeeze(1)
+			x2 = torch.stack([self.transforms(x[:,:,np.newaxis]) for x in x2], dim=0).squeeze(1)
 
 		return x1, x2, y, difference, (frame1, frame2)
 
@@ -264,7 +272,7 @@ def generate_all_offline_dataloaders(project_dir, data_dir, plots_dir, filename,
 			logging.info('Loading {} data set...'.format(dataset_type))
 			dataset = load_object(data_path.format(filename, dataset_type, num_frames_in_stack, num_pairs_per_example))
 			shuffle = 1 if dataset_type == 'train' else False
-			dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=2)
+			dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=4)
 			dataloaders.append(dataloader)
 			logging.info('Done.')
 
@@ -272,8 +280,9 @@ def generate_all_offline_dataloaders(project_dir, data_dir, plots_dir, filename,
 		logging.info('Did not find at least one data set on disk. Creating all 3...')
 
 		data_dict = {}
-		for dataset_type in ['train', 'val', 'test']:
-			data_dict[dataset_type] = load_data(project_dir, data_dir, filename, dataset_type, ext)
+		data_dict['train'], data_max, data_min = load_data(project_dir, data_dir, filename, 'train', ext)
+		for dataset_type in ['val', 'test']:
+			data_dict[dataset_type] = load_data(project_dir, data_dir, filename, dataset_type, ext, data_max, data_min)
 
 		sequence_length = data_dict['train'].shape[1]
 		max_frame_diff = np.hstack([bucket for bucket in time_buckets]).max()
@@ -303,20 +312,19 @@ def generate_all_offline_dataloaders(project_dir, data_dir, plots_dir, filename,
 				target_differences = np.append(target_differences, differences)
 				target_frame_numbers = np.vstack((target_frame_numbers, frame_numbers)) if target_frame_numbers.size else frame_numbers
 
-			stacked_img_pairs, target_buckets = torch.from_numpy(stacked_img_pairs), torch.from_numpy(target_buckets)
-			target_differences, target_frame_numbers = torch.from_numpy(target_differences), torch.from_numpy(target_frame_numbers)
 			dataset = OfflineMovingMNISTDataset(stacked_img_pairs, target_buckets, target_differences, target_frame_numbers, transforms=transforms)
 			logging.info('Done. Dumping {} data set to disk...'.format(dataset_type))
 			save_object(dataset, data_path.format(filename, dataset_type, num_frames_in_stack, num_pairs_per_example))
 			logging.info('Done.')
 
 			shuffle = True if dataset_type == 'train' else False
-			dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=2)
+			dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=4)
 			dataloaders.append(dataloader)
 
 	return dataloaders
 
-def load_data(project_dir, data_dir, filename, dataset_type, ext):
+def load_data(project_dir, data_dir, filename, dataset_type, ext, \
+			data_max=None, data_min=None):
 	filename = '{}_{}{}'.format(filename, dataset_type, ext)
 	logging.info('Loading "{}"...'.format(filename))
 	file_path = os.path.join(project_dir, data_dir, filename)
@@ -328,17 +336,27 @@ def load_data(project_dir, data_dir, filename, dataset_type, ext):
 	else:
 		raise ValueError('Unknown file extension "{}"!'.format(ext))
 	logging.info('Done.')
-	return data
+
+	# Bring data in [0,255]
+	if dataset_type == 'train' and data_max == None and data_min == None:
+		data_min, data_max = data.min(), data.max()
+		data = 255*(data - data_min)/(data_max-data_min)
+		return data.astype(np.uint8), data_max, data_min
+	else:
+		data = 255*(data - data_min)/(data_max-data_min)
+		return data.astype(np.uint8)
 
 def get_normalize_transform(data, num_frames_in_stack):
 	# Calculate mean, std from train data, and normalize
 	mean = np.mean(data)
 	std = np.std(data)
-	normalize = transforms.Compose([
-	    transforms.Normalize((mean,)*num_frames_in_stack, (std,)*num_frames_in_stack)
-	])
-	return normalize, mean, std
 
+	normalize = transforms.Compose([
+		transforms.ToTensor(),
+	    transforms.Normalize((mean,), (std,))
+	])
+
+	return normalize, mean, std
 
 def get_time_buckets_dict(time_buckets):
 	'''