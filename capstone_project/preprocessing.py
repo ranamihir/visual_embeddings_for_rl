Fixed normalization bug, refractoring.

capstone_project/preprocessing.py
@@ -27,7 +27,7 @@ class MovingMNISTDataset(Dataset):
 		video_idx = np.random.choice(len(self.data))
 		y = np.random.choice(list(self.time_buckets_dict.keys()))
 
-		(x1, x2), difference, frame_numbers = self._get_sample_at_difference(video_idx, y)
+		(x1, x2), difference, (frame1, frame2) = self._get_sample_at_difference(video_idx, y)
 
 		if self.transforms:
 			x1 = self.transforms(x1)
@@ -117,7 +117,7 @@ class AtariDataset(Dataset):
 		video_idx = np.random.choice(len(self.data))
 		y = np.random.choice(list(self.time_buckets_dict.keys()))
 
-		(x1, x2), difference, frame_numbers = self._get_sample_at_difference(video_idx, y)
+		(x1, x2), difference, (frame1, frame2) = self._get_sample_at_difference(video_idx, y)
 
 		if self.transforms:
 			x1 = self.transforms(x1)
@@ -189,7 +189,6 @@ def generate_online_dataloader(project_dir, data_dir, plots_dir, dataset, datase
 							time_buckets, batch_size, num_frames_in_stack=2, ext='.npy', \
 							transforms=None):
 	data = load_data(project_dir, data_dir, dataset, dataset_type, ext)
-	# data = np.random.uniform(size=(1, 1000, 4, 84, 84))
 
 	if 'pong' in dataset:
 		IS_STACKED_DATA = 1

capstone_project/utils.py
@@ -82,13 +82,14 @@ def accuracy(embedding_network, classification_network, dataloader, criterion, d
 	y_predicted = y_predicted.max(1)[1]
 	return 100*y_predicted.eq(y_true.data.view_as(y_predicted)).float().mean().item()
 
-def imshow(data, project_dir, plots_dir, dataset):
+def imshow(data, mean, std, project_dir, plots_dir, dataset):
 	logging.info('Plotting sample data and saving to "{}_sample.png"...'.format(dataset))
 	image_dim = data.shape[-1]
 	images = data[np.random.RandomState(0).choice(len(data), size=1)]
 	images = torch.from_numpy(images)
 
 	images = make_grid(images[0].reshape(-1, 1, image_dim, image_dim), nrow=5, padding=5, pad_value=1)
+	images = images*std + mean  # unnormalize
 	np_image = images.numpy()
 
 	fig = plt.figure(figsize=(30, 10))