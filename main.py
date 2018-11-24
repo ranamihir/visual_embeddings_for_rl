453401708dfa6eeb0ff969179ae75e82db9c8c45
Mihir Rana <ranamihir@gmail.com>
2018-11-23 21:13:49 -0500

    Separate file for retrieving args.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import argparse
 import logging
 
 import torch
@@ -11,41 +10,14 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+from capstone_project.arguments import get_args
 from capstone_project.preprocessing import generate_all_offline_dataloaders, generate_online_dataloader
 from capstone_project.models.embedding_network import EmbeddingNetwork
 from capstone_project.models.classification_network import ClassificationNetwork
 from capstone_project.utils import *
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--project-dir', metavar='PROJECT_DIR', dest='project_dir', help='path to project directory', required=False, default='.')
-parser.add_argument('--dataset', metavar='DATASET', dest='dataset', help='name of dataset file in data directory', required=False, \
-					default='mnist_test_seq')
-parser.add_argument('--data-ext', metavar='DATA_EXT', dest='data_ext', help='extension of dataset file in data directory', required=False, \
-					default='.npy')
-parser.add_argument('--data-dir', metavar='DATA_DIR', dest='data_dir', help='path to data directory (used if different from "data")', \
-					required=False, default='data')
-parser.add_argument('--offline', action='store_true', help='use offline preprocessing of data loader')
-parser.add_argument('--checkpoints-dir', metavar='CHECKPOINTS_DIR', dest='checkpoints_dir', help='path to checkpoints directory', \
-					required=False, default='checkpoints')
-parser.add_argument('--load-ckpt', metavar='LOAD_CHECKPOINT', dest='load_ckpt', help='name of checkpoint file to load', required=False)
-parser.add_argument('--batch-size', metavar='BATCH_SIZE', dest='batch_size', help='batch size', required=False, type=int, default=64)
-parser.add_argument('--epochs', metavar='EPOCHS', dest='epochs', help='number of epochs', required=False, type=int, default=10)
-parser.add_argument('--device', metavar='DEVICE', dest='device', help='device', required=False)
-parser.add_argument('--device-id', metavar='DEVICE_ID', dest='device_id', help='device id of gpu', required=False, type=int)
-parser.add_argument('--ngpu', metavar='NGPU', dest='ngpu', help='number of GPUs to use (0,1,...,ngpu-1)', required=False, type=int)
-parser.add_argument('--parallel', action='store_true', help='use all GPUs available', required=False)
-parser.add_argument('--lr', metavar='LR', dest='lr', help='learning rate', required=False, type=float, default=1e-4)
-parser.add_argument('--num-train', metavar='NUM_TRAIN', dest='num_train', help='number of training examples', required=False, \
-					type=int, default=50000)
-parser.add_argument('--num-frames', metavar='NUM_FRAMES_IN_STACK', dest='num_frames', help='number of stacked frames', required=False, \
-					type=int, default=2)
-parser.add_argument('--num-pairs', metavar='NUM_PAIRS_PER_EXAMPLE', dest='num_pairs', help='number of pairs per video', required=False, \
-					type=int, default=5)
-parser.add_argument('--use-pool', action='store_true', help='use pooling instead of strided convolutions')
-parser.add_argument('--use-res', action='store_true', help='use residual layers')
-parser.add_argument('--force', action='store_true', help='overwrites all existing dumped data sets (if used with `--offline`)')
-args = parser.parse_args()
+args = get_args()
 
 
 # Globals
